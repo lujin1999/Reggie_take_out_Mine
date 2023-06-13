7e58d0b0ea6da9824f7b42b2aaed65d0a1462595
lujin1 <1433578936@qq.com>
2023-06-13 17:41:49 +0800

    新增分类/删除分类/关联分类禁止删除

---
+++ src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
@@ -0,0 +1,21 @@
+package com.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.mapper.DishMapper;
+import com.itheima.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class DishServiceImpl extends ServiceImpl<DishMapper,Dish> implements DishService {
+
+}
---
+++ src/main/java/com/itheima/reggie/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,20 @@
+package com.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.entity.Setmeal;
+import com.itheima.reggie.mapper.SetmealMapper;
+import com.itheima.reggie.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Slf4j
+public class SetmealServiceImpl extends ServiceImpl<SetmealMapper,Setmeal> implements SetmealService {
+
+}
